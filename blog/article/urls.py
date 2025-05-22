1c980dde7b9db01b950a71e124b80d9454811104
rustam511 <rustamos1984@mail.ru>
2025-05-22 19:50:54 +0300

    style.css

--- blog/article/urls.py
+++ blog/article/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 
 from . import views
 urlpatterns = [
-     path('1/', views.article, name='article'),
+    path('1/', views.article, name='article'),
     path('2/', views.get_my_blog_posts, name='from_bd')
 ]
